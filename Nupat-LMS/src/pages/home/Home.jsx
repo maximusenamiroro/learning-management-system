2dbc12ff22796950b3db512147a7741d474a8d96
Maximus Enamiroro <brightdam8@gmail.com>
2023-02-06 11:46:55 +0100

    Signup/signin modified

--- Nupat-LMS/src/pages/home/Home.jsx
+++ Nupat-LMS/src/pages/home/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 const Home = () => {
   return (
     <div className="">
-      <p className={`text-lg text-primary-600 font-extrabold`}>hello world</p>
+      <p className={`text-lg text-primary-600 font-extrabold`}>hello CHIBUZOR</p>
     </div>
   );
 };
